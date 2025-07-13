d2b18c920adef5b2bea2fffdcb8e7a7ca3e8e07d
Muaaz Anwar <maazweber@gmail.com>
2025-07-13 11:20:33 -0700

    seeder 2

--- Lectures/database/seeders/StudentSeeder.php
+++ Lectures/database/seeders/StudentSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\student;
+use Faker\Core\File;
 
 class StudentSeeder extends Seeder
 {
@@ -18,23 +19,35 @@ class StudentSeeder extends Seeder
         //     "email"=> "Seederemail@gmail.com",
         // ]);
 
-        $student = collect([[
-            "name"=> "Seeder user 1",
-            "email"=> "1Seederemail@gmail.com"
-        ],[
-            "name"=> "Seeder user 2",
-            "email"=> "2Seederemail@gmail.com",
-        ],[
-            "name"=> "Seeder user 3",
-            "email"=> "3Seederemail@gmail.com",
-        ]]);
+        // $student = collect([[
+        //     "name"=> "Seeder user 1",
+        //     "email"=> "1Seederemail@gmail.com"
+        // ],[
+        //     "name"=> "Seeder user 2",
+        //     "email"=> "2Seederemail@gmail.com",
+        // ],[
+        //     "name"=> "Seeder user 3",
+        //     "email"=> "3Seederemail@gmail.com",
+        // ]]);
+        // $student->each(function ($student) {
+        //     student::insert($student);
+        // });
 
-        $student->each(function ($student) {
-            student::insert($student);
-        });
-        // student::create([
-        //     "name"=> "Seeder user",
-        //     "email"=> "Seederemail@gmail.com",
-        // ]);
+        // $json = file_get_contents(base_path("database/json/students.json"));
+        // $student = collect(json_decode($json));
+
+        // $student->each(function ($student) {
+        //     student::create([
+        //         "name" => $student->name,
+        //         "email" => $student->email
+        //     ]);
+        // });
+
+        for ($i = 0; $i < 10; $i++) {
+            student::create([
+                "name" => fake()->name(),
+                "email" => fake()->unique()->email()
+            ]);
+        }
     }
 }
