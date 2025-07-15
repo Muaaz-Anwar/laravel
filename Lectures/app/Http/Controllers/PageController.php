2f6f365aa2f2e21a1d2cbae10463584cd47abc7b
Muaaz Anwar <maazweber@gmail.com>
2025-07-15 03:42:29 -0700

    query

--- Lectures/app/Http/Controllers/PageController.php
+++ Lectures/app/Http/Controllers/PageController.php
@@ -13,24 +13,28 @@ class PageController extends Controller
     {
         if ($id) {
             $user = DB::table('students')->find($id);
-            return view('pages.index', ['user'=> $user, 'users'=> null]);
+            return view('pages.index', ['user'=> $user, 'value' => 1]);
         } else {
-            $users = DB::table('students')
+            // $users = DB::table('students')->whereDate('created_at', '2025-07-1')->get();
+            $users = DB::table('students')->whereBetween('id', [11,15])->get();
+            return view('pages.index', ['users'=> $users, 'value' => 0] );
             // ->where('id', $id)
             // ->where([
             // ['city', '=', ''goa],
             // ['age', '>', '21']
             // ])
             // ->wherebetween('id', [3,6])
+            // ->wherein('id', [3,6,7])
+            // ->orwherenotin('id', [3,6,7])
+            // ->wherenull('id')
+            // ->wherenotnull('id')
             // ->orwherenotbetween('id', [3,6])
             // ->where('email', 'anuemail@gmail.com')
             // ->distinct() Unique  Values
             // ->select( 'email')
-            ->get();
         // return $users;
         // dd($users);
         // dump($users);
-        return view('pages.index', ['users'=> $users, 'user'=> null] );
         }
 
 
--- Lectures/resources/views/pages/index.blade.php
+++ Lectures/resources/views/pages/index.blade.php
@@ -1,32 +1,66 @@
 @extends('template_components.layout')
 
 
-    @section('content')
-        <h1>This is index template page</h1>
-        <table width="100%">
-            <tr>
-                <td><h3>Name</h3></td>
-                <td><h3>Email</h3></td>
-            </tr>
-            @if ($users != '')
-            @foreach ($users as $user)
-            <tr style="height:40px;">
-                <td>{{ $user->name }}</td>
-                <td>{{ $user->email }}</td>
-                <td><a style="width:160px !important;padding:10px 50px; background-color:rgb(0, 255, 0);border-radius:6px; color:black" href="controller/{{ $user->id }}">{{ $user->id }}</a></td>
-            </tr>
-            @endforeach
-            @endif
-            @if ($user)
-            <tr style="height:40px;">
-                <td>{{ $user->name }}</td>
-                <td>{{ $user->email }}</td>
-                <td><a style="width:160px !important;padding:10px 50px; background-color:rgb(0, 255, 0);border-radius:6px; color:black" href="{{ route('p_home') }}">Back</a></td>
-            </tr>
-            @endif
+@section('content')
+<h1>This is index template page</h1>
+<table width="100%">
+    <tr>
+        <td>
+            <h3>Id</h3>
+        </td>
+        <td>
+            <h3>Name</h3>
+        </td>
+        <td>
+            <h3>Email</h3>
+        </td>
+        <td>
+            <h3>Created at</h3>
+        </td>
+        <td>
+            <h3>Updated at</h3>
+        </td>
+        <td>
+            <h3>Show More</h3>
+        </td>
+    </tr>
+    @if (!$value)
+    @if ($users->count() >= 1)
+    @foreach ($users as $user)
+    <tr style="height:40px;">
+        <td>{{ $user->id }}</td>
+        <td>{{ $user->name }}</td>
+        <td>{{ $user->email }}</td>
+        <td>{{ $user->created_at }}</td>
+        <td>{{ $user->updated_at }}</td>
+        <td><a style="width:160px !important;padding:10px 50px; background-color:rgb(0, 255, 0);border-radius:6px; color:black"
+                href="controller/{{ $user->id }}">Show</a></td>
+    </tr>
+    @endforeach
+    @else
+    <tr>
+        <td colspan="6" align="center" style="background-color:red; padding-top:20px">
+            <h2>No data Found</h2>
+        </td>
+    </tr>
+    @endif
 
-        </table>
 
-        </ol>
-        <br>
-    @endsection
+    @else
+    <tr style="height:40px;">
+        <td>{{ $user->id }}</td>
+        <td>{{ $user->name }}</td>
+        <td>{{ $user->email }}</td>
+        <td>{{ $user->created_at }}</td>
+        <td>{{ $user->updated_at }}</td>
+        <td><a style="width:160px !important;padding:10px 50px; background-color:rgb(0, 255, 0);border-radius:6px; color:black"
+                href="{{ route('p_home') }}">Back</a></td>
+    </tr>
+    @endif
+
+
+</table>
+
+</ol>
+<br>
+@endsection
