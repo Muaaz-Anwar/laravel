056dc8ecd9548e85c2e36ec71eda89807358f4bd
Muaaz Anwar <maazweber@gmail.com>
2025-07-29 04:56:43 +0500

    with softdelete

--- learning_api/app/Http/Controllers/PostController.php
+++ learning_api/app/Http/Controllers/PostController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controllers\HasMiddleware;
 use Illuminate\Routing\Controllers\Middleware;
 use Illuminate\Support\Facades\Gate;
+use PhpParser\JsonDecoder;
 
 class PostController extends Controller implements HasMiddleware
 {
@@ -76,6 +77,6 @@ class PostController extends Controller implements HasMiddleware
     {
         Gate::authorize('modify', $post);
         $post->delete();
-        return ['message' , 'Deleted'];
+        return ['message' , "deleted"];
     }
 }
