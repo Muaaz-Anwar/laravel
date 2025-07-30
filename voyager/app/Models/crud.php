c3c104e94ebfb58ffd9cc37b17610a4acdd83fd0
Muaaz Anwar <maazweber@gmail.com>
2025-07-30 14:00:50 +0500

    completed voyager crud

---
+++ voyager/app/Models/crud.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class crud extends Model
+{
+    use HasFactory;
+    protected $table = "crud";
+    protected $fillable = [
+        "title","body"
+    ];
+}
